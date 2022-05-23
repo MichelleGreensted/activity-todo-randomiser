e2ede9946ae7c8c975f55ffe0e77e160ceda1c10
MichelleGreensted <71762549+MichelleGreensted@users.noreply.github.com>
2022-05-23 16:11:07 +0100

    Added css and corresponding classNames to align ActivityCard elements

--- src/components/ActivityCard.tsx
+++ src/components/ActivityCard.tsx
@@ -2,14 +2,19 @@ import "./ActivityCard.css"
 
 export function ActivityCard() {
     return <div className="container">
-        <input type="checkbox" className="checkbox"/>
-        <div className="activity-card-text">
-            <h3>Activity Name</h3>
-            <p>Location</p>
+        <div className="holders">
+            <input type="checkbox" className="checkbox"/>
         </div>
-        <div>
-            <div/>
-            <div/>
+        <div className="holder1">
+            <div className="name-and-location holders">
+                <h3>Activity Name</h3>
+                <p>Location</p>
+            </div>
+            <div className="holders weather-and-delete">
+                <input type="checkbox" className="weather-checkbox"/>
+                <button>Delete</button>
+            </div>
         </div>
+        
     </div>
 }
