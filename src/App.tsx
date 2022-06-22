e6d713c1cf1824688ca432533880bcfce949ab51
MichelleGreensted <71762549+MichelleGreensted@users.noreply.github.com>
2022-06-22 11:50:32 +0200

    Added Modal component and basic Show Modal function

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import './App.css';
-import { ActivitiesList } from './components/ActivitiesList';
-import { GetActivity } from './components/GetActivity';
+import "./App.css";
+import { ActivitiesList } from "./components/ActivitiesList";
+import { GetActivity } from "./components/GetActivity";
+import { Modal } from "./components/modal/Modal";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Activity Todo Randomiser
-      </header>
-      <GetActivity/>
-      <ActivitiesList/>
+      <header className="App-header">Activity Todo Randomiser</header>
+      <GetActivity />
+      <ActivitiesList />
+      <Modal />
     </div>
   );
 }
